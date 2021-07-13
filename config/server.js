a104c0a39cceeb78e19e0711ce93b4520aeb577d
anonymous <anonymous>
2021-07-13 07:28:29 +0200

    SURAPP-293: Adjust the usage of the secrets

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '683a5177860e449a5cd931834657b846'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
