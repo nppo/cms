cc4a717baad022d65473aacd64d516f7d801a935
anonymous <anonymous>
2021-07-14 07:19:58 +0200

    SURAPP-293: Configure email plugin better

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  email: {
+    provider: env('MAIL_DRIVER', 'sendmail'),
+    providerOptions: {
+      apiKey: env('MAILGUN_API_KEY'),
+      domain: env('MAILGUN_DOMAIN'),
+      host: env('MAILGUN_HOST', 'api.eu.mailgun.net'),
+    },
+    settings: {
+      defaultFrom: env('MAIL_FROM_ADDRESS'),
+    },
+  },
+});
